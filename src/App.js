1c6ab7d73be9bf58b9f7d662348fde7dfd87dca8
Parinya Onsuwan <parinya.ons@gmail.com>
2019-06-11 10:03:46 +0700

    Using css from guideline, add example cell component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Cell extends Component {
+    state = {
+        number: 1
+    }
+    render() {
+        return (
+            <div
+                class={`cell ${this.props.isInitial ? 'initial' : ''}`}
+                onClick={(e) => {
+                    this.setState({
+                        number: (this.state.number + 1) % 5
+                    })
+                }}
+                >
+                { this.state.number ? this.state.number : '' }
+            </div>)
+    }
+}
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="board">
+                    <Cell number={4} />
+                    <Cell number={3} isInitial />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                    <Cell number={3} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
