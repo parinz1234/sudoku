9032e72690c33f650167aa297a51d60425d6f39e
Parinya Onsuwan <parinya.ons@gmail.com>
2019-06-11 23:33:23 +0700

    Add change number function and dynamic number

--- src/App.js
+++ src/App.js
@@ -2,45 +2,62 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Cell extends Component {
-    state = {
-        number: 1
-    }
     render() {
         return (
             <div
-                class={`cell ${this.props.isInitial ? 'initial' : ''}`}
+                className={`cell ${this.props.isInitial ? 'initial' : ''}`}
                 onClick={(e) => {
-                    this.setState({
-                        number: (this.state.number + 1) % 5
-                    })
+                    if (this.props.isInitial) return
+                    this.props.onChange((this.props.number + 1) % 5)
                 }}
-                >
-                { this.state.number ? this.state.number : '' }
+            >
+                {this.props.number ? this.props.number : ''}
             </div>)
+    }
+}
+
+class Board extends Component {
+    state = {
+        board: [
+            [1, 2, 3, 4],
+            [3, 4, 0, 0],
+            [2, 0, 4, 0],
+            [4, 0, 0, 2]
+        ],
+        initial: [
+            [true, true, true, true],
+            [true, true, false, false],
+            [true, false, true, false],
+            [true, false, false, true]
+        ]
+    }
+    render() {
+        return (
+            <div className="board">
+                {
+                    this.state.board.map((row, i) => row.map((number, j) =>
+                        <Cell
+                            key={`cell-${i}-${j}`}
+                            number={number}
+                            isInitial={this.state.initial[i][j]}
+                            onChange={(newNumber) => {
+                                const { board } = this.state
+                                board[i][j] = newNumber
+                                this.setState({
+                                    board
+                                })
+                            }}
+                            />))
+                }
+            </div>
+        )
     }
 }
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="board">
-                    <Cell number={4} />
-                    <Cell number={3} isInitial />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                    <Cell number={3} />
-                </div>
+                <Board />
             </div>
         );
     }
